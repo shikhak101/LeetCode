dd2ed5f08159ab9de57f77d27e5c7f6b81aa59c8
Shikha Kumari <shikha.jsr26@gmail.com>
2019-09-08 05:21:33 -0700

    Add files via upload

---
+++ RemoveLinkedListElements.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while(curr != NULL) {
+            if(curr->val == val) {
+                if(curr == head){
+                    head = curr->next;
+                }
+                else {
+                    prev->next = curr->next;
+                }
+            }
+            else {
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
